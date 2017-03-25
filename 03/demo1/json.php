b0cd23b93e16cb1e31ebb5afa4e57cea1857b7fd
wanawan <18733521820@163.com>
2017-03-25 23:04:29 +0800

    react简单应用

---
+++ 03/demo1/json.php
@@ -0,0 +1,3 @@
+<?php
+echo '{"name":"Tom","age":19,"sex":"男"}';
+?>
---
+++ 03/demo2/json.php
@@ -0,0 +1,3 @@
+<?php
+echo '[{"name":"Tom","age":19,"sex":"男"},{"name":"Jarry","age":21,"sex":"女"},{"name":"Susan","age":19,"sex":"女"}]';
+?>
